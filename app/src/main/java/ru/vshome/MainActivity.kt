2ef9847341941500ad314df9afb62c2ab0bf8b52
vladislavsumin <vladislavsumin@mail.ru>
2021-08-10 14:16:06 +0300

    Setup basic navigation

--- app/src/main/java/ru/vshome/MainActivity.kt
+++ app/src/main/java/ru/vshome/MainActivity.kt
@@ -5,6 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import ru.vshome.ui.screens.servers.ServersScreen
 import ru.vshome.ui.theme.VSHomeTheme
 
@@ -12,12 +17,25 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            VSHomeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    ServersScreen()
-                }
-            }
+            Themed()
         }
+    }
+}
+
+@Composable
+private fun Themed() {
+    VSHomeTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background) {
+            Navigation()
+        }
+    }
+}
+
+@Composable
+private fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "servers") {
+        composable("servers") { ServersScreen() }
     }
 }
